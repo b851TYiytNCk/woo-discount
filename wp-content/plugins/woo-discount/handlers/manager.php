e8eeb0710dfed2ab26d5236daa72937b9eb95eb1
Vladyslav Nahornyi <vladyslav@carcloud.com>
2024-04-01 23:15:22 +0300

    Initial commit

---
+++ wp-content/plugins/woo-discount/handlers/manager.php
@@ -0,0 +1,10 @@
+<?php
+/* ACTIVATION */
+function woo_discount_activate() {
+
+}
+
+/* DEACTIVATION */
+function woo_discount_deactivate() {
+
+}
---
+++ wp-content/plugins/woo-discount/woo-discount.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Dynamic Discount
+ * Plugin URI: https://github.com/b851TYiytNCk/woo-discount
+ * Description: Automatic dynamic discount extension for WooCommerce
+ * Version: 1.0.0
+ * Requires PHP: 7.4
+ * Author: Vladyslav Nahornyi
+ * Author URI: https://github.com/b851TYiytNCk
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: woo-discount
+ *
+ */
+
+$handler = __DIR__  . '/handlers/manager.php';
+
+if ( file_exists($handler) ) {
+	register_activation_hook( $handler, 'woo_discount_activate' );
+	register_deactivation_hook( $handler, 'woo_discount_deactivate' );
+} else {
+	throw new \RuntimeException('Plugin files are missing, try reinstalling the plugin' );
+}
