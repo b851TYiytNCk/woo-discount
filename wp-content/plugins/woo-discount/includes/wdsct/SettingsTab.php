817c86e3c69c33d246cd1c70c1b265eff220ba81
Vladyslav Nahornyi <vladyslav@carcloud.com>
2024-04-24 22:34:46 +0300

    refactor settings management

--- wp-content/plugins/woo-discount/includes/wdsct/SettingsTab.php
+++ wp-content/plugins/woo-discount/includes/wdsct/SettingsTab.php
@@ -3,13 +3,25 @@
 namespace wdsct;
 
 class SettingsTab {
+	public $tab_name = 'wdsct_tab';
+
 	public function __construct() {
 		// Add a custom tab to the WooCommerce settings
-		add_filter( 'woocommerce_settings_tabs_array', array( $this, 'wdsct_woo_settings_tab' ), 50 );
+		add_filter(
+			'woocommerce_settings_tabs_array',
+			array( $this, 'wdsct_woo_settings_tab' ),
+			50
+		);
 		// Display the content of the custom tab
-		add_action( 'woocommerce_settings_tabs_wdsct_tab', array( $this, 'wdsct_woo_settings_content' ) );
+		add_action(
+			"woocommerce_settings_tabs_$this->tab_name",
+			array( $this, 'wdsct_output_settings_content' )
+		);
 		// Save custom settings
-		add_action( 'woocommerce_update_options_wdsct_tab', array( $this, 'save_wdsct_woocommerce_settings' ) );
+		add_action(
+			"woocommerce_update_options_$this->tab_name",
+			array( $this, 'wdsct_save_settings' )
+		);
 	}
 
 	public function wdsct_woo_settings_tab( $settings_tabs ) {
@@ -17,15 +29,31 @@ class SettingsTab {
 		return $settings_tabs;
 	}
 
-	public function wdsct_woo_settings_content(): void {
-		// Start Tab content
-		include_once( PLUGIN_DIR . '/templates/settings/table.php' );
+	private function settings() {
+
+		$settings = array(
+			array(
+				'name' => __( 'Dynamic Discount', 'wdsct'),
+				'type' => 'title',
+			),
+			array(
+				'id'       => 'wdsct-enable-toggle',
+				'name'     => 'Enable dynamic discounts',
+				'type'     => 'checkbox',
+			),
+			array(
+				'type' => 'sectionend',
+			)
+		);
+		return $settings;
+
 	}
 
-	public function save_wdsct_woocommerce_settings(): void {
-		// Save settings
+	public function wdsct_output_settings_content(): void {
+		\WC_Admin_Settings::output_fields( $this->settings() );
 	}
 
-
-
+	public function wdsct_save_settings(): void {
+		\WC_Admin_Settings::save_fields( $this->settings() );
+	}
 }
